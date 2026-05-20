eb59c99248af4636ed33bca2a7b33d780514f986
Süleymanov Nəsib <72444222+nasibius@users.noreply.github.com>
2026-05-20 14:23:19 +0400

    tasks

--- network_security/2x00_network_fundamentals/6-broadcast.sh
+++ network_security/2x00_network_fundamentals/6-broadcast.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IFS=. read -r a b c d <<< "$1"; IFS=. read -r e f g h <<< "$2"; echo "$((a | (e ^ 255))).$((b | (f ^ 255))).$((c | (g ^ 255))).$((d | (h ^ 255)))"
+IFS=. read -r a b c d <<< "$1"; IFS=. read -r e f g h <<< "$2"; printf "%s" "$((a | (e ^ 255))).$((b | (f ^ 255))).$((c | (g ^ 255))).$((d | (h ^ 255)))"
